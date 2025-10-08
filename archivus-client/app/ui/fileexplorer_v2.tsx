89070da5cdece8688a1e0ab2131b4fc5f8c721e9
samar <scipioresearch2020@gmail.com>
2025-10-08 15:34:25 +0530

    dnd working with click

--- archivus-client/app/ui/fileexplorer_v2.tsx
+++ archivus-client/app/ui/fileexplorer_v2.tsx
@@ -14,12 +14,22 @@ import {
 } from '@dnd-kit/sortable';
 
 import FileFolderDialog from "./components/fileFolderModal";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+
 
 const FileDraggableCard: React.FC<{
     file: FileMetaData;
     onClick: (file: FileMetaData) => void;
 }> = ({file, onClick}) => {
-    const {attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({id: file.ID});
+    const {attributes, listeners, setNodeRef, transform, transition, isDragging} = useSortable({id: file.id});
     const style:React.CSSProperties  = {
         transform: CSS.Transform.toString(transform),
         transition: transition || "transform 200ms ease", // 👈 fallback
@@ -49,15 +59,16 @@ const FileDraggableCard: React.FC<{
 
 }
 const generateRandomId = () => {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    return "gid_"+Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
 export default function FileExplorer() {
       const searchParams = useSearchParams();
       const folder = searchParams.get('folder') || '';
-        const [files, setFiles] = useState<FileMetaData[]>([]);
+        const [files, setFiles] = useState<Map<string,FileMetaData>>(new Map<string, FileMetaData>());
         const [loading, setLoading] = useState<boolean>(false);
         const [size, setSize] = useState<number>(0);
+        const [activeId, setActiveId] = useState<string | null>(null);
         
       useEffect(() => {
         async function loadFiles() {
@@ -70,8 +81,11 @@ export default function FileExplorer() {
                       }
                       file.id = generateRandomId();
                     }
-                    setFiles(data.files || []);
+                    let fileMap = new Map<string,FileMetaData>();
+                    data.files.forEach((f) => fileMap!.set(f.id,f));
+                    setFiles(fileMap);
                     setSize(data.size);
+                    console.log(fileMap);
                   } catch (err) {
                     console.error("Error fetching files", err);
                   } finally {
@@ -86,31 +100,69 @@ export default function FileExplorer() {
         useSensor(MouseSensor));
 
     const handleDragStart = (event: DragStartEvent) => {
-        console.log("drag start", event.active.id);
+        // console.log("drag start", event.active.id);
+        setActiveId(event.active.id.toString());
     };
-        const handleDragEnd = (event: DragEndEvent) => {
-            const {active, over} = event;
-            console.log("Drag ended:", {active, over});
-        }
-        const handleClick = (file: FileMetaData) => {
-            console.log("Clicked file:", file);
+    const handleDragEnd = (event: DragEndEvent) => {
+        const {active, over} = event;
+        // console.log("Drag ended:", {active, over});
+        if (!over ||active.id == over.id) return;
+        const activeFile = files.get(active.id.toString());
+        if (activeFile && activeFile.IsDir) {
+          alert("Cannot move folders");
+          setActiveId(null);
+          return;
         }
 
+        const overFile = files.get(over.id.toString());
+        if (overFile?.IsDir) {
+          moveFile(activeFile!.Path, overFile.Path).then((res) => {
+            if (res) {
+              alert("File moved successfully");
+              window.location.reload();
+            }
+          }).catch((err) => {
+            console.error("Error moving file", err);
+            alert("Error moving file");
+          });
+          setActiveId(null);
+          return;
+        }
+
+    }
+    const handleClick = (file: FileMetaData) => {
+        if (file.IsDir) {
+            window.location.href = `?folder=${file.Path}`;
+        } else {
+            window.open(file.SignedUrl, '_blank');
+        }
+    }
+
       return (
-        // <FileFolderDialog folder={folder} />
+        <>
+        <FileFolderDialog folder={folder} />
         <DndContext
       sensors={sensors}
       collisionDetection={closestCenter}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
     >
-      <SortableContext items={files.map((f) => f.id)} strategy={verticalListSortingStrategy}>
-        <div className="flex flex-col gap-3 p-4 w-full max-w-md mx-auto">
-          {files.map((file,indx) => (
-            <FileDraggableCard key={indx} file={file} onClick={handleClick} />
+      <SortableContext items={Array.from(files!.values())} strategy={verticalListSortingStrategy}>
+        <ul role="list" className="grid grid-cols-6 gap-6 sm:grid-cols-4 lg:grid-cols-7">
+          {Array.from(files!.values()).map((file) => (
+            <FileDraggableCard key={file.id} file={file} onClick={handleClick} />
           ))}
-        </div>
+        </ul>
+          <DragOverlay>
+                {activeId ? (
+                  <div className="p-3 bg-white border rounded-md shadow-lg scale-105 opacity-90">
+                    <FileDraggableCard key={activeId} file={files!.get(activeId)!} onClick={() => {}}/>
+                  </div>
+                ) : null}
+          </DragOverlay>
+        
       </SortableContext>
     </DndContext>
+    </>
       )
 }
